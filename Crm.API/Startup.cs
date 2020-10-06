af38ad0220c54568b248b9aa7f5b52c78902e303
Salih Cantekin <salih.cantekin@etstur.com>
2020-10-06 12:11:10 +0300

    aa

--- Crm.API/Startup.cs
+++ Crm.API/Startup.cs
@@ -38,8 +38,6 @@ namespace Crm.API
             });
 
             services.AddHttpContextAccessor();
-
-
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
