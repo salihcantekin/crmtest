8963540b3ece0b9c5b5e83cbc9839f0052a7f1d7
Salih Cantekin <salih.cantekin@etstur.com>
2020-10-07 15:19:15 +0300

    DockerCompose file changed

--- Crm.API/Startup.cs
+++ Crm.API/Startup.cs
@@ -45,7 +45,7 @@ namespace Crm.API
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseHttpsRedirection();
+            //app.UseHttpsRedirection();
 
             app.UseRouting();
 
